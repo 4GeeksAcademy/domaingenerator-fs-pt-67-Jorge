7506cb8a4ff6e96335b0ec8fb2d0bc98dcafccc6
8u1137pr00f <162195925+8u1137pr00f@users.noreply.github.com>
2024-05-31 08:21:42 +0000

    Update: añadido array dominios [] + corrección con el .join  para que acomode y muestre el resultado de todo en la home

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ let pronoun = ["I", "You", "He", "She", "We"];
 let adj = ["Big", "Small", "Fast", "Beautiful", "Smart"];
 let noun = ["Dog", "Cat", "Table", "City", "Mountain"];
 let dom = [".com", ".es", ".net", ".org", ".us"];
+let dominios = [];
 
 window.onload = function() {
   for (let i = 0; i < pronoun.length; i++) {
@@ -16,12 +17,11 @@ window.onload = function() {
       for (let k = 0; k < noun.length; k++) {
         for (let c = 0; c < dom.length; c++) {
           console.log(pronoun[i] + adj[j] + noun[k] + dom[c]);
-          //declarar array vacío dentro de la función para ponerle dentro los arrays que hayamos generales
-          const array = [];
-          array.push(pronoun[i] + adj[j] + noun[k] + dom[c]);
+          //declarar array vacío dentro de la función para ponerle dentro los arrays que hagamos generales
+          dominios.push(pronoun[i] + adj[j] + noun[k] + dom[c]);
         }
       }
     }
   }
-  document.getElementById("dominios").innerHTML = dominios;
+  document.getElementById("dominios").innerHTML = dominios.join("<br>");
 };
